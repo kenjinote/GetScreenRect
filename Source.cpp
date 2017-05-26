4491733ef12420ab1800705632bc718dc0d89b38
kenjinote <kenji256@gmail.com>
2017-05-26 16:29:12 +0900

    マルチディスプレイ対応

--- Source.cpp
+++ Source.cpp
@@ -4,32 +4,49 @@
 
 TCHAR szClassName[] = TEXT("Window");
 
+BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
+{
+	MONITORINFOEX MonitorInfoEx;
+	MonitorInfoEx.cbSize = sizeof(MonitorInfoEx);
+	if (GetMonitorInfo(hMonitor, &MonitorInfoEx) != 0)
+	{
+		DEVMODE dm = { 0 };
+		dm.dmSize = sizeof(DEVMODE);
+		if (EnumDisplaySettings(MonitorInfoEx.szDevice, ENUM_CURRENT_SETTINGS, &dm) != 0)
+		{
+			const int nMonitorWidth = dm.dmPelsWidth; // プライマリモニターの横幅（ピクセル数）
+			const int nMonitorHeight = dm.dmPelsHeight;  // プライマリモニターの縦幅（ピクセル数）
+			TCHAR szText[1024];
+			wsprintf(szText, TEXT("【画面解像度】\r\n幅 = %dピクセル\r\n高さ = %dピクセル\r\n\r\n"), nMonitorWidth, nMonitorHeight);
+			SendMessage((HWND)dwData, EM_REPLACESEL, 0, (LPARAM)szText);
+		}
+	}
+	return TRUE;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	static HWND hEdit;
 	static HWND hButton;
+	static HWND hEdit;
 	switch (msg)
 	{
 	case WM_CREATE:
-		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
 		hButton = CreateWindow(TEXT("BUTTON"), TEXT("再取得"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | WS_VSCROLL | ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE | ES_READONLY, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
 		SendMessage(hWnd, WM_COMMAND, IDOK, 0);
 		break;
-	case WM_SETTINGCHANGE:
+	case WM_DISPLAYCHANGE:
 		SendMessage(hWnd, WM_COMMAND, IDOK, 0);
 		break;
 	case WM_SIZE:
-		MoveWindow(hEdit, 10, 10, 256, 128, TRUE);
-		MoveWindow(hButton, 10, 148, 256, 32, TRUE);
+		MoveWindow(hButton, 10, 10, 256, 32, TRUE);
+		MoveWindow(hEdit, 10, 50, 256, 256, TRUE);
 		break;
 	case WM_COMMAND:
 		if (LOWORD(wParam) == IDOK)
 		{
-			TCHAR szText[1024];
-			const DWORD dwWidth = GetSystemMetrics(SM_CXSCREEN);
-			const DWORD dwHeight = GetSystemMetrics(SM_CYSCREEN);
-			wsprintf(szText, TEXT("画面解像度\r\n\r\n幅 = %dピクセル\r\n高さ = %dピクセル"), dwWidth, dwHeight);
-			SetWindowText(hEdit, szText);
+			SetWindowText(hEdit, 0);
+			EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM)hEdit);
 		}
 		break;
 	case WM_DESTROY:
